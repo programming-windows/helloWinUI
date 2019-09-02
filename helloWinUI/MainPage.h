68b681860d77ac8dc93662326dc24bc2a2670b17
jichenghu <jicheng@yahoo.com>
2019-09-02 09:51:41 +0800

    Add project files.

---
+++ helloWinUI/MainPage.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "MainPage.g.h"
+#include "winrt/Microsoft.UI.Xaml.Controls.h"
+#include "winrt/Microsoft.UI.Xaml.XamlTypeInfo.h"
+
+namespace winrt::helloWinUI::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::helloWinUI::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
